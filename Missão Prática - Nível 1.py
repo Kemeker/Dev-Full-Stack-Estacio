85afe7fb489c5b02b3108951d524ee1f8012c0a8
Kemeker <kemeker@gmail.com>
2022-08-28 01:26:33 -0300

    primeira missao

--- Missão Prática - Nível 1.py
+++ Missão Prática - Nível 1.py
@@ -1,10 +1,15 @@
-num1 = float(input('Digite um numero: '))
-num2 = float(input('Digite um numero: '))
-soma = (num1 + num2)
+# Atividade realizada em python porem nao ficou 100%
 
-print('A soma dos numeros é: ', soma)
+primos=[]
+n = int(input("Digite um valor: "))
 
-if soma <= 25:
-    print('O numero ', soma, " é menor ou igual a 25")
-    if soma > 25:
-        print('O numero ', soma, " é maior que 25")
+for x in range (17,n):
+    cont=0
+    
+    for y in range(1, x+1):
+        if x%y==0:
+            cont+=1
+    if cont<=2:
+        primos.append(x)  
+
+print("Os numeros ", primos, " sao primos")
