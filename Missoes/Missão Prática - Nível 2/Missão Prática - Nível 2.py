ef71d681ec891c428503c92d2ba6a7c7802cb368
Kemeker <kemeker@gmail.com>
2022-09-03 23:09:00 -0300

    Alteraçoes nos codigos das atividade nivel 2

--- Missoes/Missão Prática - Nível 2/Missão Prática - Nível 2.py
+++ Missoes/Missão Prática - Nível 2/Missão Prática - Nível 2.py
@@ -2,21 +2,31 @@
 
 #ATIVIDADE VALIDA PARA MISSAO NIVEL 2
 
+n = int(input("Digite um numero da base decimal: "))
+numero_digitado = n
+quociente = 1
+lista = []
+
+while quociente >= 1:
+  resto = n%2
+  lista.insert(0,resto)
+  quociente = n // 2
+  n = quociente
+
+binario = ''.join([str(item) for item in lista])
+print(f"O número {numero_digitado}, quando convertido em binário vale: {binario}")
+
+
+
+# OUTRA MANEIRAS MAIS SIMPLES 
+"""
 n = int(input("Entre com um numero: "))
 n = bin(n)
 print("O numero convertido em binario é: ",n[2::])
-
-
-
-
-
-
-
-
 """
-Tentativa utilizando outro metado, desconsiderar para atividade
-Apenas estudo
-"""
+
+
+#OUTRA MANEIRA DE FAZER A CONVERSAO
 """"
 decimal = int(input("Digite um numero decimmal: "))
 binaria = "{0:b}" .format(decimal)
