8ccff0011fa132174586c0bcaceb49dcfa4d8bf3
Kemeker <kemeker@gmail.com>
2022-09-04 01:54:29 -0300

    teste em script para exercicio nivel 3

--- Atividades Aulas/Python Basico/PythonBasico.py
+++ Atividades Aulas/Python Basico/PythonBasico.py
@@ -1,2 +1,10 @@
-lista= ["cachorro", "hamster", ["pato", "galinha", "porco"],"gato"]
-print(lista[3][2])
+# CONJUNTO A = {0,1,2,3,4,5} FORMULA 2 elevaado a n.
+#n é o numero de elementos de  um  conjunto
+
+print("Calcule o conjunto das partes do\nConjunto A=[0, 1, 2, 3, 4, 5] ")
+
+conjunto_partes = 2**6
+
+print(f"O conjunto das partes do   conjunto A=[0, 1, 2, 3, 4, 5] é:\n{conjunto_partes} ")
+
+
