68d904510b777000ce8bbe892dc9ae149c0522f2
Kemeker <kemeker@gmail.com>
2022-09-18 00:29:31 -0300

    Update PythonBasico.py

--- Atividades Aulas/Python Basico/PythonBasico.py
+++ Atividades Aulas/Python Basico/PythonBasico.py
@@ -1,10 +1,7 @@
 # CONJUNTO A = {0,1,2,3,4,5} FORMULA 2 elevaado a n.
 #n é o numero de elementos de  um  conjunto
 
-print("Calcule o conjunto das partes do\nConjunto A=[0, 1, 2, 3, 4, 5] ")
 
-conjunto_partes = 2**6
 
-print(f"O conjunto das partes do   conjunto A=[0, 1, 2, 3, 4, 5] é:\n{conjunto_partes} ")
 
 
