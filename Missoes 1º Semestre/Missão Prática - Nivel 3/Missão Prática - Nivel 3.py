ea13ff98671059ef18e05d87e31176eff4688b23
Kemeker <kemeker@gmail.com>
2022-09-26 02:43:43 -0300

    atividade pratica 3 concluida

--- Missoes 1º Semestre/Missão Prática - Nivel 3/Missão Prática - Nivel 3.py
+++ Missoes 1º Semestre/Missão Prática - Nivel 3/Missão Prática - Nivel 3.py
@@ -2,20 +2,17 @@
 #n é o numero de elementos de  um  conjunto
 
 
-import math
+#A soma dos elementos de um conjunto sempre vai ser
+# 2 elevado a n ( numero de elementos)
+conjunto_A = {0,1,2,3,4,5}
 
-#conjunto = {0,1,2,3,4,5}
+# Estrutura de loop, vai percorer a range de 0 ate o numero maximo de elementos 
+# dentro do conjunto.
+#Variavel soma vai fazer 2 elevado ao numero total de elementos dos conjunto
+for conjunto in range(0, len(conjunto_A)):
+    soma = 2 ** len(conjunto_A)
+
+print(f"Este conjunto tem {len(conjunto_A)} elementos")
+print(f"E as partes deste conjunto é igual a {soma}")
 
 
-
-#somaConjunto = []
-
-
-cont = 0
-while cont < 6:
-    cont +=1
-    soma = cont
-
-    
-soma = 2 ** cont
-print(soma)
