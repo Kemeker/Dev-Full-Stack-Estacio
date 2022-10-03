9d35dd8e1313e29ea389a738edfc1ab7ad14801a
Kemeker <kemeker@gmail.com>
2022-10-03 17:25:51 -0300

    Correçao na atividade

--- Missoes 1º Semestre/Missão Prática - Nivel 3/Missão Prática - Nivel 3.py
+++ Missoes 1º Semestre/Missão Prática - Nivel 3/Missão Prática - Nivel 3.py
@@ -1,18 +1,13 @@
-# CONJUNTO A = {0,1,2,3,4,5} FORMULA 2 elevaado a n.
-#n é o numero de elementos de  um  conjunto
+def subconjutos(numeros):
+    return subconjutos_recursivo([], sorted(numeros))
 
+def subconjutos_recursivo(atual, conjunto):
+    if conjunto:
+        return subconjutos_recursivo(atual, conjunto[1:]) + subconjutos_recursivo(atual + [conjunto[0]], conjunto[1:])
+    return [atual]  
 
-#A soma dos elementos de um conjunto sempre vai ser
-# 2 elevado a n ( numero de elementos)
-conjunto_A = {0,1,2,3,4,5}
+numeros = [1,2,3]
+resultado = subconjutos(numeros)
 
-# Estrutura de loop, vai percorer a range de 0 ate o numero maximo de elementos 
-# dentro do conjunto.
-#Variavel soma vai fazer 2 elevado ao numero total de elementos dos conjunto
-for conjunto in range(0, len(conjunto_A)):
-    soma = 2 ** len(conjunto_A)
-
-print(f"Este conjunto tem {len(conjunto_A)} elementos")
-print(f"E as partes deste conjunto é igual a {soma}")
-
-
+print(f"O numero de elementos do conjunto {numeros} é: {len(resultado)}")
+print(f"As partes do conjunto {numeros} são: {resultado}")
