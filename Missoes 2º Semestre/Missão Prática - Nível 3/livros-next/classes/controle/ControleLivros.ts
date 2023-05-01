1f5e1eba5e40c0b2056b4072c6352c5e73bb46b0
Kemeker <kemeker@gmail.com>
2023-05-01 12:35:24 -0300

    Adicionando diretorio classes

---
+++ Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/classes/controle/ControleLivros.ts
@@ -0,0 +1,55 @@
+import Livro from "../modelo/Livro"
+
+
+// Define a variável livros como um Array<Livro>
+const livros: Array<Livro> = [
+    {
+        codigo: 1,
+        codEditora: 1,
+        titulo: "Livro 1",
+        resumo: "Resumo do Livro 1",
+        autores: ["Autor 1", "Autor 2"],
+
+    },
+    {
+        codigo: 2,
+        codEditora: 2,
+        titulo: "Livro 2",
+        resumo: "Resumo do Livro 2",
+        autores: ["Autor 3"],
+    },
+    {
+        codigo: 3,
+        codEditora: 3,
+        titulo: "Livro 3",
+        resumo: "Resumo do Livro 3",
+        autores: ["Autor 4", "Autor 5", "Autor 6"],
+    },
+]
+
+class ControleLivros{
+  // Implementa o método obterLivros, com o retorno do vetor livros
+  obterLivros(): Array<Livro> {
+    return livros
+  }
+  
+  // Implementa o método incluir, recebendo um objeto do tipo Livro e adicionando-o ao vetor de livros
+  incluir(Livro: Livro): void{
+    const novoCodigo = livros.reduce((max, livro) => Math.max(max, livro.codigo), 0) + 1;
+    livros.push({ ...livro, codigo: novoCodigo });
+  }
+
+   // Implementa o método excluir, removendo o livro com o código fornecido do vetor de livros
+   excluir(codigo: number): void {
+    const index = livros.findIndex((livro) => livro.codigo === codigo);
+    if (index !== -1) {
+      livros.splice(index, 1);
+    }
+  }
+}
+
+export default ControleLivros
+
+
+
+    
---
+++ Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/classes/modelo/Editora.ts
@@ -0,0 +1,12 @@
+class Editora{
+    codEditora: number
+    nome: string
+
+    constructor(codEditora: number, nome:string){
+        this.codEditora = codEditora
+        this.nome = nome
+
+ }      
+}
+
+export default Editora;
