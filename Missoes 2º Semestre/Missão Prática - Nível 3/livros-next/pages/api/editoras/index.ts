ddd192f1cb10d6e5061a5b8b70a0b3414ff63627
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-05-10 04:07:31 -0300

    atualizaçao

--- Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/pages/api/editoras/index.ts
+++ Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/pages/api/editoras/index.ts
@@ -1,16 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import controleEditoras from '../../../classes/controle/ControleEditoras'
+import ControleEditoras from '../../../classes/controle/ControleEditoras'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
     try {
-      const editoras = controleEditoras.getEditoras()
-      res.status(200).json(editoras)
+      const controleEditoras = new ControleEditoras();
+      const editoras = controleEditoras.getEditoras();
+      res.status(200).json(editoras);
     } catch (error) {
-      res.status(500).json({ error: 'Erro interno do servidor' })
+      res.status(500).json({ error: 'Erro interno do servidor' });
     }
   } else {
-    res.setHeader('Allow', 'GET')
-    res.status(405).json({ error: 'Método não permitido' })
+    res.setHeader('Allow', 'GET');
+    res.status(405).json({ error: 'Método não permitido' });
   }
 }
+
+
+
+
+
+
