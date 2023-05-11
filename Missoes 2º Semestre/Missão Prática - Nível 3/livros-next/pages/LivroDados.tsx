d949faee60131286c6ce39e532a7694f1e61f9e2
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-05-11 19:46:39 +0000

    novos arquivos criados

---
+++ Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/pages/LivroDados.tsx
@@ -0,0 +1,4 @@
+import type { NextPage } from 'next';
+import React from 'react';
+
+const LivroDados: NextPage = () => { return (<main>Ola Mundo</main>)}
---
+++ Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/pages/LivroLista.tsx
@@ -0,0 +1,4 @@
+import type { NextPage } from 'next';
+import React from 'react';
+
+const LivroLista: NextPage = () => { return (<main>Ola Mundo</main>)}
