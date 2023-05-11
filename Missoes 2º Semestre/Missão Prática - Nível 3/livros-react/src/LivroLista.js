8edebbae43fcc2327100e8057ec015a546527f9d
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-05-11 03:32:50 -0300

    up

--- Missoes 2º Semestre/Missão Prática - Nível 3/livros-react/src/LivroLista.js
+++ Missoes 2º Semestre/Missão Prática - Nível 3/livros-react/src/LivroLista.js
@@ -2,80 +2,64 @@ import React, { useState, useEffect } from 'react';
 import ControleLivros from './controle/ControleLivros';
 import ControleEditoras from './controle/ControleEditoras';
 
-
-function LinhaLivro(props) {
-  const { livro, excluir } = props;
-  const controleEditora = new ControleEditora();
-  const nomeEditora = controleEditora.getNomeEditora(livro.codEditora);
-
-  function handleExcluir() {
-    excluir(livro.codigo);
-  }
-
-  return (
-    <tr>
-      <td>
-        <button onClick={handleExcluir}>Excluir</button>
-      </td>
-      <td>{livro.codigo}</td>
-      <td>{livro.titulo}</td>
-      <td>{livro.resumo}</td>
-      <td>{nomeEditora}</td>
-      <td>
-        <ul>
-          {livro.autores.map((autor, index) => (
-            <li key={index}>{autor}</li>
-          ))}
-        </ul>
-      </td>
-    </tr>
-  );
-}
-
-
-
-
-const controleLivro = new ControleLivros();
-const controleEditora = new ControleEditoras();
-
-
 const LivroLista = () => {
   const [livros, setLivros] = useState([]);
   const [carregado, setCarregado] = useState(false);
 
-  const excluirLivro = (codigo) => {
-    controleLivro.excluir(codigo);
-    setCarregado(false); // Força o redesenho da página após a exclusão
-  };
+  const controleLivro = new ControleLivros();
+  const controleEditora = new ControleEditoras();
 
   useEffect(() => {
-    const obterLivros = () => {
-      const livros = controleLivro.obterLivros();
+    const carregarLivros = async () => {
+      const livros = await controleLivro.obterLivros();
       setLivros(livros);
       setCarregado(true);
-
-    }
+    };
 
     if (!carregado) {
       carregarLivros();
     }
   }, [carregado]);
 
-  function handleExcluirLivro(codigo) {
+  const excluirLivro = (codigo) => {
     controleLivro.excluir(codigo);
-    setCarregado(false);
-  }
+    setCarregado(false); // Força o redesenho da página após a exclusão
+  };
+
+  const LinhaLivro = ({ livro }) => {
+    const nomeEditora = controleEditora.getNomeEditora(livro.codEditora);
+
+    const handleExcluirLivro = () => {
+      excluirLivro(livro.codigo);
+    };
+
+    return (
+      <tr>
+        <td>
+          <button onClick={handleExcluirLivro}>Excluir</button>
+        </td>
+        <td>{livro.codigo}</td>
+        <td>{livro.titulo}</td>
+        <td>{livro.resumo}</td>
+        <td>{nomeEditora}</td>
+        <td>
+          <ul>
+            {livro.autores.map((autor, index) => (
+              <li key={index}>{autor}</li>
+            ))}
+          </ul>
+        </td>
+      </tr>
+    );
+  };
 
   return (
     <main>
       <h1>Lista de Livros</h1>
-      <button className="btn btn-danger mb-4" onClick={() => handleExcluirLivro()}>
-        Excluir Livro
-      </button>
       <table>
         <thead>
           <tr>
-            <th></th>
+            <th>Ações</th>
             <th>Código</th>
             <th>Título</th>
             <th>Resumo</th>
@@ -85,69 +69,11 @@ const LivroLista = () => {
         </thead>
         <tbody>
           {livros.map((livro) => (
-            <LinhaLivro key={livro.codigo} livro={livro} excluir={handleExcluirLivro} />
+            <LinhaLivro key={livro.codigo} livro={livro} />
           ))}
         </tbody>
       </table>
     </main>
-  );
-}
-
-
-
-    obterLivros();
-  }, []);
-
-  const LinhaLivro = ({ livro }) => {
-    const nomeEditora = controleEditora.getNomeEditora(livro.codEditora);
-
-    return (
-      <tr>
-        <td>
-          <button onClick={() => excluirLivro(livro.codigo)}>Excluir</button>
-        </td>
-        <td>{livro.codigo}</td>
-        <td>{livro.titulo}</td>
-        <td>{livro.resumo}</td>
-        <td>
-          <ul>
-            {livro.autores.map((autor, index) => (
-              <li key={index}>{autor}</li>
-            ))}
-          </ul>
-        </td>
-        <td>{nomeEditora}</td>
-      </tr>
-    );
-  };
-
-  return (
-    <div>
-      <main>
-        <h1>Livros</h1>
-        {carregado ? (
-          <table>
-            <thead>
-              <tr>
-                <th>Ações</th>
-                <th>Código</th>
-                <th>Título</th>
-                <th>Resumo</th>
-                <th>Autores</th>
-                <th>Editora</th>
-              </tr>
-            </thead>
-            <tbody>
-              {livros.map((livro) => (
-                <LinhaLivro key={livro.codigo} livro={livro} />
-              ))}
-            </tbody>
-          </table>
-        ) : (
-          <p>Carregando livros...</p>
-        )}
-      </main>
-    </div>
   );
 };
 
