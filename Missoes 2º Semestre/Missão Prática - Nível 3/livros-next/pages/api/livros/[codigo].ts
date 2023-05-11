8e48615479608c6c69f1a6fd242e3c1babb72e32
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-05-11 18:37:49 +0000

    up

---
+++ Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/pages/api/livros/[codigo].ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import ControleLivros from '../../../classes/controle/ControleLivros';
+
+const controleLivro = new ControleLivros();
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  
+}
--- Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/pages/api/livros/index.ts
+++ Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/pages/api/livros/index.ts
@@ -4,8 +4,23 @@ import ControleLivros from '../../../classes/controle/ControleLivros';
 const controleLivro = new ControleLivros();
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'GET') {
-    const livros = controleLivro.obterLivros();
-    res.status(200).json(livros);
+  try {
+    if (req.method === 'GET') {
+      const livros = controleLivro.obterLivros();
+      res.status(200).json(livros);
+    } else if (req.method === 'POST') {
+      const livro = req.body;
+      controleLivro.incluir(livro);
+      res.status(200).json({ message: 'Livro adicionado com sucesso.' });
+    } else if (req.method === 'DELETE') {
+      const codigo = Number(req.query.codigo);
+      controleLivro.excluir(codigo);
+      res.status(200).json({ message: 'Livro excluído com sucesso.' });
+    } else {
+      res.status(405).json({ error: 'Método não permitido.' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Ocorreu um erro no servidor.' });
   }
 }
+
