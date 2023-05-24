1b4aa08ff8ef29b15258a92a5bb9296bc195fad9
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-05-23 23:51:41 -0300

    finnalizado atividade 3

--- Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/componentes/LinhaLivro.tsx
+++ Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/componentes/LinhaLivro.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ControleEditoras from '../classes/controle/ControleEditoras';
 import Livro from '../classes/modelo/Livro';
+import styles from '../styles/Home.module.css';
+import Head from 'next/head';
+import { Menu } from '../componentes/Menu';
 
 const controleEditoras = new ControleEditoras();
+const baseURL = 'http://localhost:3000/api/livros';
 
 interface LinhaLivroProps {
   livro: Livro;
   excluir: () => void;
 }
 
-export const LinhaLivro: React.FC<LinhaLivroProps> = ({ livro, excluir }) => {
+const LinhaLivro: React.FC<LinhaLivroProps> = ({ livro, excluir }) => {
   const editora = controleEditoras.obterEditoraPorCodigo(livro.codEditora);
 
   return (
-    <tr>
+    <tr className={styles.container}>
       <td>{livro.codigo}</td>
       <td>{livro.titulo}</td>
       <td>{livro.resumo}</td>
@@ -28,5 +32,63 @@ export const LinhaLivro: React.FC<LinhaLivroProps> = ({ livro, excluir }) => {
   );
 };
 
+const LivroLista: React.FC = () => {
+  const [livros, setLivros] = useState<Array<Livro>>([]);
+  const [carregado, setCarregado] = useState(false);
 
+  const obterLivros = async () => {
+    await fetch(baseURL)
+      .then((response) => response.json())
+      .then((data) => setLivros(data))
+      .catch((error) => console.log(error));
+    setCarregado(true);
+  };
 
+  const excluirLivro = async (codigo: number) => {
+    await fetch(`${baseURL}/${codigo}`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.ok)
+      .then(() => setCarregado(false))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    obterLivros();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>LivroLista</title>
+      </Head>
+      <Menu />
+      <main className={styles.main}>
+        <h1 className={styles.title}>Lista de Livros</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Código</th>
+              <th>Título</th>
+              <th>Resumo</th>
+              <th>Autores</th>
+              <th>Editora</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {livros.map((livro) => (
+              <LinhaLivro
+                key={livro.codigo}
+                livro={livro}
+                excluir={() => excluirLivro(livro.codigo)}
+              />
+            ))}
+          </tbody>
+        </table>
+      </main>
+    </div>
+  );
+};
+
+export default LivroLista;
--- Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/pages/LivroDados.tsx
+++ Missoes 2º Semestre/Missão Prática - Nível 3/livros-next/pages/LivroDados.tsx
@@ -1,4 +1,114 @@
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useState } from 'react';
+import styles from '../styles/Home.module.css';
+import ControleEditora from '../classes/controle/ControleEditoras';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 
-const LivroDados: NextPage = () => { return (<main>Ola Mundo</main>)}
+const controleEditora = new ControleEditora();
+const baseURL = 'http://localhost:3000/api/livros';
+
+interface Livro {
+  codigo: number;
+  titulo: string;
+  resumo: string;
+  autores: string[];
+  codEditora: number;
+}
+
+const LivroDados: NextPage = () => {
+  const router = useRouter();
+
+  const incluirLivro = async (livro: Livro) => {
+    await fetch(baseURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(livro),
+    })
+      .then((response) => response.ok)
+      .catch((error) => console.log(error));
+  };
+
+  const opcoes = controleEditora.getEditoras().map((editora) => ({
+    value: editora.codEditora,
+    text: editora.nome,
+  }));
+
+  const [titulo, setTitulo] = useState('');
+  const [resumo, setResumo] = useState('');
+  const [autores, setAutores] = useState('');
+  const [codEditora, setCodEditora] = useState(opcoes[0].value);
+
+  const tratarCombo = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setCodEditora(Number(event.target.value));
+  };
+
+  const incluir = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const livro: Livro = {
+      codigo: 0,
+      titulo: titulo,
+      resumo: resumo,
+      autores: autores.split('\n'),
+      codEditora: codEditora,
+    };
+
+    incluirLivro(livro).then(() => {
+      router.push('/LivroLista');
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>LivroDados</title>
+      </Head>
+      <header>
+        <nav>Menu</nav>
+      </header>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Inclusão de Livro</h1>
+        <form onSubmit={incluir}>
+          <div>
+            <label>Título:</label>
+            <input
+              type="text"
+              value={titulo}
+              onChange={(event) => setTitulo(event.target.value)}
+            />
+          </div>
+          <div>
+            <label>Resumo:</label>
+            <textarea
+              value={resumo}
+              onChange={(event) => setResumo(event.target.value)}
+            ></textarea>
+          </div>
+          <div>
+            <label>Autores:</label>
+            <textarea
+              value={autores}
+              onChange={(event) => setAutores(event.target.value)}
+            ></textarea>
+          </div>
+          <div>
+            <label>Editora:</label>
+            <select value={codEditora} onChange={tratarCombo}>
+              {opcoes.map((opcao) => (
+                <option key={opcao.value} value={opcao.value}>
+                  {opcao.text}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button type="submit">Incluir</button>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default LivroDados;
